f8a932f8ac718f2071ebb65c3dd6f9a99d571a23
henrik <2cXaJAyd>
2020-06-17 12:04:26 +0200

    Added layer profillinje.

--- load_tolkn_layers.py
+++ load_tolkn_layers.py
@@ -150,8 +150,9 @@ class LoadLayers():
 
         #special fix for gvflode
         self.create_layer_value_relations(layer_dict['gvflode'], layer_dict['zz_gvmag'], layer_dict['gvflode'].dataProvider().fieldNameIndex('intermag'), 'typ','beskrivning')
-        for layer_projekt in ['profillinje']: #, 'profil' # Implementera vid behov
-            self.create_layer_value_relations(layer_dict[layer_projekt], layer_dict['zz_projekt'], layer_dict[layer_projekt].dataProvider().fieldNameIndex('projekt'), 'pkuid','namn')
+        for projektdependent_layer in ['profillinje']: #, 'profil' # Implementera vid behov
+            if projektdependent_layer in layer_dict:
+                self.create_layer_value_relations(layer_dict[projektdependent_layer], layer_dict['zz_projekt'], layer_dict[projektdependent_layer].dataProvider().fieldNameIndex('projekt'), 'pkuid','namn')
 
         #last, rename to readable names in map legend
         for layer in layer_list:
